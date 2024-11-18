a0ed8cd3c101ad7f0ac0c256aad07eee08801e2d
Your Namehuzaifa <you@example.com>
2024-11-18 22:43:25 +0500

    first commit

--- app/component/navbar.tsx
+++ app/component/navbar.tsx
@@ -8,7 +8,7 @@ export const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <div className='bg-[#E2F1E7] z-0 sticky top-0'>
+    <div className='bg-[#E2F1E7] z-50 sticky top-0'>
       <header className="text-gray-600 body-font ">
         <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
           <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
